348d0cdc929d55948b3b42a76a1dedd38c1e81dd
Chris Hodges <chris.hodges@adtran.com>
2019-09-02 18:22:27 -0500

    Add settings button

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-main',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./main.component.css'],
 })
 export class MainComponent {
-  constructor() { }
+  constructor(private router: Router) { }
+
+  settingsClick() {
+    localStorage.clear();
+    this.router.navigate(['settings']);
+  }
 }
