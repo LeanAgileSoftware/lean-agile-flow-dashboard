a912269bccbd990b2358658875d66fe279130230
Chris Hodges <chris.hodges@gmail.com>
2020-03-02 22:12:13 -0600

    Work towards green test for pull request table (#88)

--- src/app/pull-request-table/pull-request-table.component.spec.ts
+++ src/app/pull-request-table/pull-request-table.component.spec.ts
@@ -1,22 +1,43 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import {MatTableModule} from '@angular/material/table';
+import { MatGridListModule,
+         MatCardModule,
+         MatMenuModule,
+         MatIconModule,
+         MatButtonModule,
+         MatToolbarModule,
+         MatInputModule,
+         MatFormFieldModule,
+         } from '@angular/material';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { UserSettingsService } from '../user-settings.service';
+import { GithubService } from '../github.service';
 import { PullRequestTableComponent } from './pull-request-table.component';
+import { TimeAgoPipe } from 'time-ago-pipe';
 
 xdescribe('PullRequestTableComponent', () => {
   let component: PullRequestTableComponent;
   let fixture: ComponentFixture<PullRequestTableComponent>;
+  let spyGithub: jasmine.SpyObj<GithubService>;
 
   beforeEach(async(() => {
+    const mockGithubProvider = jasmine.createSpyObj('GithubService', ['verifyConnection', 'getPullRequestsForUsers']);
     TestBed.configureTestingModule({
-      declarations: [ PullRequestTableComponent ],
-      imports: [MatTableModule,
+      declarations: [ PullRequestTableComponent, TimeAgoPipe ],
+      imports: [MatGridListModule,
+                MatTableModule,
+                MatMenuModule,
+                MatIconModule,
+                MatCardModule,
+                MatButtonModule,
+                MatToolbarModule,
+                MatInputModule,
                 BrowserAnimationsModule],
-      providers: [UserSettingsService]
+      providers: [UserSettingsService, {provide: GithubService, useValue: mockGithubProvider}]
     })
     .compileComponents();
+    spyGithub = TestBed.get(GithubService);
   }));
 
   beforeEach(() => {
